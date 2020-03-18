c500894c6616eb9e8903f51d3e86438020acc505
Rosivania <rosivania.soaresdasilva@gmail.com>
2020-03-17 23:07:43 -0300

    Aula 02 Data:17/03/2020

--- aula02/src/index.js
+++ aula02/src/index.js
@@ -1,4 +1,4 @@
 import ArrecadacaoDao from './model/dao/ArrecadacaoDao';
 
 let dao = new ArrecadacaoDao();
-dao.carregarArrecadacoesDosMunicipios();
+dao.carregarArrecadacoesPorAno(1996);
--- aula02/src/model/dao/ArrecadacaoDao.js
+++ aula02/src/model/dao/ArrecadacaoDao.js
@@ -3,22 +3,16 @@ import path from 'path';
 import excelToJsan from 'convert-excel-to-json';
 
 import Municipio from '../entity/Municipio';
+import {ANOS} from '../../constants/Colunas';
 
 export default class Arrecadacao {
 
     carregarArrecadacoesDosMunicipios() {
         let municipios = [];
 
-        /**
-         * aponta para o arquivo
-         * da planilha de dados
-         */
         let caminhoArquivo = path.resolve(__dirname, '../../data/dados.xls');
 
-        let dados = excelToJsan({
-            source: fs.readFileSync(caminhoArquivo),
-            range: 'A4:P185'
-        });
+        let dados = this.extrairDadosDaPlanilha('A4:P185');
 
         dados.Plan1.forEach(elemento => {
             let nome = elemento.A;
@@ -34,5 +28,27 @@ export default class Arrecadacao {
         });
 
         return municipios;
+    }
+
+    carregarArrecadacoesPorAno(ano){
+        if(ano in ANOS){
+            let coluna = ANOS[ano];
+            let range = `${coluna}4:${colina}185`;
+
+            let arrecadacoes = this.extrairDadosDaPlanilha(range);
+            let nomeMunicipios = this.extrairDadosDaPlanilha('A4:A185');
+            console.log(arrecadacoes, nomeMunicipios);
+        }
+    }
+
+    extrairDadosDaPlanilha(range){
+        let caminhoArquivo = path.resolve(__dirname, '../../data/dados.xls');
+
+        let dados = excelToJsan({
+            source: fs.readFileSync(caminhoArquivo),
+            range
+        });
+        return dados;
+
     }
 }
